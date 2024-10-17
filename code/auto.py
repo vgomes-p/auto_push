b6fb4facd9b9275bf34bc2904b36a4873d240393
Vinicius Eduardo Gomes Pereira <vgomes-p@c1r6p4.42sp.org.br>
2024-10-17 19:59:46 -0300

    commit_00

---
+++ code/auto.py
@@ -0,0 +1,48 @@
+import pyautogui as at
+import time as tm
+
+at.PAUSE = .5
+
+def actenter(void):
+	at.write(void)
+	at.press('enter')
+
+def maxsr():
+	at.keyDown("win")
+	at.press("up")
+	at.keyUp("win")
+
+def splitsr():
+	at.keyDown("win")
+	at.press("right")
+	at.keyUp("win")
+
+def minsr():
+	at.keyDown("alt")
+	at.keyDown("space")
+	at.press("n")
+	at.keyUp("alt")
+	at.keyUp("space")
+
+#open terminal
+at.press("win")
+actenter("terminal")
+actenter("fish")
+tm.sleep(1)
+actenter("clear")
+
+#open direct
+actenter("cd /nfs/homes/vgomes-p/42/42zip/libft_hub")
+
+#git status
+actenter("git status")
+tm.sleep(1.5)
+
+#git add
+actenter("git add .")
+
+#git commit
+actenter('git commit -m "auto_commit_by_vgomes-p_py"')
+
+#git push
+actenter('git push')
